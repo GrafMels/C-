2112c781e1c2574d30ed8c90224e6323a804a338
Mels <ilia2577@gmail.com>
2022-08-09 16:54:55 +0800

    Lesson 2

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = { 1, 2, 4, 5, 6, 4, 8};
+
+int lenghtArray = array.Length;
+int find = 4;
+
+int index = 0;
+
+while(index < lenghtArray){
+    if(array[index] == find){
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
