f84c0d1c37725a1a3d346deccd1ad41a35ac9931
Mels <ilia2577@gmail.com>
2022-08-09 14:21:17 +0800

    Refresh_4 Seminar_1

--- Seminar_1/Program_3/Program.cs
+++ Seminar_1/Program_3/Program.cs
@@ -1,17 +1,37 @@
-﻿Console.WriteLine("Введите номер дня недели");
+﻿// Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
+Console.WriteLine("Введите номер дня недели");
 int dayOfTheWeek = Convert.ToInt32(Console.ReadLine());
-if(dayOfTheWeek = 1){
-Console.WriteLine("1 - Понедельник");
-}if(dayOfTheWeek = 1){
-Console.WriteLine("2 - Вторник");
-}if(dayOfTheWeek = 1){
-Console.WriteLine("3 - Среда");
-}if(dayOfTheWeek = 1){
-Console.WriteLine("4 - Четверг");
-}if(dayOfTheWeek = 1){
-Console.WriteLine("5 - Пятница");
-}if(dayOfTheWeek = 1){
-Console.WriteLine("6 - Суббота");
-}else{
-Console.WriteLine("7 - Воскресенье");
+if(dayOfTheWeek == 1)
+{
+    Console.WriteLine("1 - Понедельник");
+}
+if(dayOfTheWeek == 2)
+{
+    Console.WriteLine("2 - Вторник");
+}
+if(dayOfTheWeek == 3)
+{
+    Console.WriteLine("3 - Среда");
+}
+if(dayOfTheWeek == 4)
+{
+    Console.WriteLine("4 - Четверг");
+}
+if(dayOfTheWeek == 5)
+{
+    Console.WriteLine("5 - Пятница");
+}
+if(dayOfTheWeek == 6)
+{
+    Console.WriteLine("6 - Суббота");
+}
+if(dayOfTheWeek == 7)
+{
+    Console.WriteLine("7 - Воскресенье");
+}
+else
+{
+    Console.Write("Такого дня недели не бывает");
 }
---
+++ Seminar_1/Program_5/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// 2 -> " -2, -1, 0, 1, 2"
+Console.Write("Введите положительное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int negativeNumber = number * (-1);
+
+while(negativeNumber <= number){
+    Console.Write(negativeNumber + ", ");
+    negativeNumber++;
+}
