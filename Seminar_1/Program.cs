3254ae105ab0a7fbe78ccdf8958a95be22168bb8
Mels <ilia2577@gmail.com>
2022-08-09 12:59:01 +0800

    Repush Seminar_1

--- Seminar_1/Program.cs
+++ Seminar_1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
+
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+Console.Write("Введите число которое надо возвести в квадрат:");
+int first_number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Квадрат этого числа:");
+Console.WriteLine(first_number*first_number);
+//Или второй вариант решение через подключение библиотеки Math
+double sqrNumber = Math.Pow(first_number, 2);
+Console.WriteLine("Квадрат этого числа " + first_number + " = " + sqrNumber);
+
+
+
