71b7b96f0d58b287fc9628f94bfdc57c754af45f
Mels <ilia2577@gmail.com>
2022-08-09 13:56:46 +0800

    Refresh_3 Seminar_1

---
+++ Seminar_1/Program_3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите номер дня недели");
+int dayOfTheWeek = Convert.ToInt32(Console.ReadLine());
+if(dayOfTheWeek = 1){
+Console.WriteLine("1 - Понедельник");
+}if(dayOfTheWeek = 1){
+Console.WriteLine("2 - Вторник");
+}if(dayOfTheWeek = 1){
+Console.WriteLine("3 - Среда");
+}if(dayOfTheWeek = 1){
+Console.WriteLine("4 - Четверг");
+}if(dayOfTheWeek = 1){
+Console.WriteLine("5 - Пятница");
+}if(dayOfTheWeek = 1){
+Console.WriteLine("6 - Суббота");
+}else{
+Console.WriteLine("7 - Воскресенье");
+}
