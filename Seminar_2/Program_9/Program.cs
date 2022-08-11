8816562e60150157b30447460bd3f61e7adbd2dd
Mels <ilia2577@gmail.com>
2022-08-11 11:28:05 +0800

    new Program_9 and new Seminar_2

---
+++ Seminar_2/Program_9/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая выводит случайное число из отрезка 
+//[10, 99] и показывает максимальную  из двух цифр
+// 78 -> 8
+// 12 -> 2
+// 85 -> 8
+int number = new Random().Next(10, 100);
+Console.WriteLine(number);
+if((number/10) < (number%10)){
+    Console.WriteLine("Max digit = " + number%10);
+}else if((number/10) > (number%10)){
+    Console.WriteLine("Max digit = " + number/10);
+}else{
+    Console.WriteLine("Both digit are equal = " + number%10);
+}
