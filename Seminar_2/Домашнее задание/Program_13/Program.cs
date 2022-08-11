63a7bb91708a649830caa09d5ddcea1d6687fb15
Mels <ilia2577@gmail.com>
2022-08-11 13:58:50 +0800

    Неправильное решение задачи 13 в домашней работе плюс новое решение задачи 11 в семинаре

---
+++ Seminar_2/Домашнее задание/Program_13/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+int number = new Random().Next(1, 10000);
+string numberStrg = Convert.ToString(number);
+if(number > 99) Console.WriteLine($"К числу {numberStrg}, ответ: {numberStrg[2]}");
+if(number < 99) Console.WriteLine($"К числу {numberStrg}, третьей цифры нет");
---
+++ Seminar_2/Домашнее задание/Program_15/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+Console.Write("Введите номер дня недели а я скажу выходной он или нет: ");
+int dayOfTheWeek = Convert.ToInt32(Console.ReadLine());
+if (dayOfTheWeek < 1 || dayOfTheWeek > 7) Console.WriteLine("Такого дня недели не существует");
+if (dayOfTheWeek == 1 || dayOfTheWeek == 2 || dayOfTheWeek == 3 || dayOfTheWeek == 4 || dayOfTheWeek == 5) Console.WriteLine("Это НЕ выходной");
+if (dayOfTheWeek == 6 || dayOfTheWeek == 7) Console.WriteLine("Это ВЫХОДНОЙ! УРА!");
