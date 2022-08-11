7fb271e59e58a0cfb73b0db9ab532df00efe6e56
Mels <ilia2577@gmail.com>
2022-08-11 14:09:08 +0800

    Complete Homework

--- Seminar_2/Домашнее задание/Program_13/Program.cs
+++ Seminar_2/Домашнее задание/Program_13/Program.cs
@@ -5,8 +5,8 @@
 // 78 -> третьей цифры нет
 
 // 32679 -> 6
-
-int number = new Random().Next(1, 10000);
+Console.Write("Введите число, а я вам скажу какая у него третья цифра: ");
+int number = Convert.ToInt32(Console.ReadLine());
 string numberStrg = Convert.ToString(number);
 if(number > 99) Console.WriteLine($"К числу {numberStrg}, ответ: {numberStrg[2]}");
 if(number < 99) Console.WriteLine($"К числу {numberStrg}, третьей цифры нет");
