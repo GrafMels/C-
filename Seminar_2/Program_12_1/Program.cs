96bfcee3017b83c0fbbff7ccaf78b09903d156a0
Mels <ilia2577@gmail.com>
2022-08-11 13:22:23 +0800

    New Program_12_1

---
+++ Seminar_2/Program_12_1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите два числа через Enter");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+int min = Math.Min(firstNumber, secondNumber);
+if(min*min == firstNumber){
+    Console.WriteLine($"{firstNumber} является квадратом числа, {secondNumber}");
+}else if(min*min == secondNumber){
+    Console.WriteLine($"{secondNumber} является квадратом числа, {firstNumber}");
+}else{
+    Console.WriteLine("Ниодно число не является квадартом второго");
+}
