bc43642342d7b3c82eab5ab9ce31fce7bd80df41
Mels <ilia2577@gmail.com>
2022-08-15 11:17:27 +0800

    End Program_17

--- Seminar_3/Program_17/Program.cs
+++ Seminar_3/Program_17/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  17. Напишите программу, которая принимает на
+//  вход координаты точки (X и Y), причём Х ≠ 0 и
+//  Y ≠ 0 и выдаёт номер четверти плоскости, 
+//  в которой находится эта точка
+
+Console.Write("Введите значение кординаты оси x: ");
+int x = int.Parse(Console.ReadLine());
+Console.Write("Введите значение кординаты оси y: ");
+int y = int.Parse(Console.ReadLine());
+
+if (x > 0 && y > 0) Console.WriteLine("Это первая четверть");
+if (x < 0 && y > 0) Console.WriteLine("Это вторая четверть");
+if (x < 0 && y < 0) Console.WriteLine("Это третья четверть");
+if (x > 0 && y < 0) Console.WriteLine("Это четвёртая четверть");
