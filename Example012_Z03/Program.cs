a809666eee8016c154fb752dd6fd3c05ba5fcac5
Mels <ilia2577@gmail.com>
2022-08-17 16:05:25 +0800

    Lesson003: Сhanges

---
+++ Example012_Z03/Program.cs
@@ -0,0 +1,40 @@
+﻿// программа для сортировки масиива по возрастанию
+
+int[] arr = new int[30];
+
+//раскоментируй что бы заполнить масиив случайными числами
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = new Random().Next(1, 10);
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
