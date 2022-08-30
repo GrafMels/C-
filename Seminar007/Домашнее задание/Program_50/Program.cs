f5a657d1d172197694f2d72d69204d486e12c552
Mels <ilia2577@gmail.com>
2022-08-30 11:44:33 +0800

    Seminar007: Homework: End

---
+++ Seminar007/Домашнее задание/Program_50/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 17 -> такого числа в массиве нет
+
+Console.Write("Введите колличество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите колличество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] MakeRandArray(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = new Random().Next(10);
+        }
+    }
+    return matrix;
+}
+
+void PrintArray (int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] array = MakeRandArray(rows, columns);
+PrintArray(array);
+
+Console.WriteLine("Введите индекс строки искомого числа: ");
+int indexRows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите индекс столбца искомого числа: ");
+int indexColumns = Convert.ToInt32(Console.ReadLine());
+if(indexRows > rows || indexColumns > columns || indexRows < 0 || indexColumns < 0) System.Console.WriteLine($"Элемента в {indexRows}-ой строке и {indexColumns}-ом столбце не существует");
+else Console.WriteLine($"Элемент в {indexRows}-ой строке и {indexColumns}-ом столбце: {array[indexRows,indexColumns]}");
+
---
+++ Seminar007/Домашнее задание/Program_52/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.Write("Введите колличество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите колличество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] MakeRandArray(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = new Random().Next(10);
+        }
+    }
+    return matrix;
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[] GetSumColumns(int[,] matrix)
+{
+    double[] sum = new double[matrix.GetLength(1)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+        sum[i] += matrix[j, i];
+        if (j == matrix.GetLength(1)-1) sum[i] /= (j+1);
+    }
+    }
+return sum;
+}
+
+int[,] array = MakeRandArray(rows, columns);
+PrintArray(array);
+
+double[] summ = GetSumColumns(array);
+
+Console.WriteLine();
+Console.WriteLine();
+for (int i = 0; i < array.GetLength(1); i++)
+{
+Console.Write($"{summ[i]}\t");  
+}
+
